951fb1637afd1b16587e813c92db5ee6cbcb0836
Daniele <marini.daniele98@gmail.com>
2025-02-05 16:27:00 +0100

    Enhance shopping cart summary to display item prices and total price

--- src/app/components/shopping-cart-summary/shopping-cart-summary.component.ts
+++ src/app/components/shopping-cart-summary/shopping-cart-summary.component.ts
@@ -13,14 +13,17 @@ export class ShoppingCartSummaryComponent implements OnChanges {
   @Input('cart') cart!: ShoppingCart
   cartArray: any
   public shoppingCartItemCount = 0
+  public shoppingTotalPrice = 0
 
   ngOnChanges(changes: SimpleChanges){
     if(changes['cart'] && this.cart.items){
       this.cartArray = Object.values(this.cart.items)
       this.shoppingCartItemCount = this.cartArray.reduce((total: number, product: any) => total + (product.quantity || 0), 0)
+      this.shoppingTotalPrice = this.cartArray.reduce((total: number, product: any) => total + (product.quantity * product.price || 0), 0)
 
       console.log("Aggiornato cartArray:", this.cartArray);
       console.log("Totale prodotti nel carrello:", this.shoppingCartItemCount)
+      console.log("Totale prezzo prodotti nel carrello:", this.shoppingTotalPrice)
     }
   }
 
